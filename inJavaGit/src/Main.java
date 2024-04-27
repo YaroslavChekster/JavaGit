c5deffa472944b9b21bebfa28b6c8b6cf09375fb
n.yarek <yaroslav.chexter@gmail.com>
2024-04-27 09:51:46 +0300

    add pablic static int num()

--- inJavaGit/src/Main.java
+++ inJavaGit/src/Main.java
@@ -1,5 +1,19 @@
+import java.util.Scanner;
+
 public class Main {
     public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
         System.out.println("Hello world!");
+
+        int num1 = sc.nextInt();
+        int num2 = sc.nextInt();
+        System.out.println(num(num1, num2));
+    }
+
+    public static int num(int num1, int num2) {
+        for (int i = 0; i < 10; i++) {
+            num1 += num2 * num1 / 10 + num1 *2;
+        }
+        return num1;
     }
 }
